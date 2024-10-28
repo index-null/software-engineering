dd744152d12731842278e741273aa1cb55c054ec
Luolimasi0v0 <kidneyhshd@qq.com>
2024-10-28 13:22:18 +0800

    feat(models): 重构模型并支持 PostgreSQL
    - 更新 go.mod 文件，添加 PostgreSQL 驱动依赖
    - 重构 image 和 user 模型，将UserQuery结构体从user.go转移到image.go，并将image结构体改为FavoritedImage
    - 修改 init.go 中的 SQL 语句

--- backend/text-to-picture/models/image/image.go
+++ backend/text-to-picture/models/image/image.go
@@ -1,8 +1,21 @@
-package models
+package image
 
-type image struct {
-	ID     int       `json:"id" gorm:"primarykey"`
-	UserID string    `json:"user_id" gorm:"not null"`
-	Result string    `json:"result"`
-	User   UserLogin `gorm:"foreignKey:UserID;references:ID"`
+import "gocode/backend/backend/text-to-picture/models/user"
+
+//用户历史查询
+type UserQuery struct {
+	ID       int            `json:"id" gorm:"primarykey"`
+	UserName int            `json:"user_name" gorm:"not null"`
+	Params   string         `json:"params"`
+	Result   string         `json:"result"`
+	Time     string         `json:"time"`
+	User     user.UserLogin `gorm:"foreignKey:UserName;references:UserName"`
+}
+
+//用户收藏查询
+type FavoritedImage struct {
+	ID       int            `json:"id" gorm:"primarykey"`
+	UserName string         `json:"user_name" gorm:"not null"`
+	Result   string         `json:"result"`
+	User     user.UserLogin `gorm:"foreignKey:UserName;references:UserName"`
 }
