331a652c5ba309886bd0d9ee91649dee647600bd
lind003 <127481264+lind003@users.noreply.github.com>
2024-10-29 00:11:31 +0800

    Update RegisterView.vue
    添加用户名和密码长度限制，重置功能修改完毕

--- frontend/text2image-vue/src/views/RegisterView.vue
+++ frontend/text2image-vue/src/views/RegisterView.vue
@@ -1,65 +1,90 @@
 <template>
-    <div class="register-container">
-      <el-form ref="registerForm" :model="registerForm" label-width="100px">
-        <el-form-item label="用户名">
-          <el-input v-model="registerForm.username"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="registerForm.email" type="email"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input type="password" v-model="registerForm.password"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码">
-          <el-input type="password" v-model="registerForm.confirmPassword"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm">注册</el-button>
-          <el-button @click="resetForm">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        registerForm: {
-          username: '',
-          email: '',
-          password: '',
-          confirmPassword: ''
-        }
-      };
-    },
-    methods: {
-      submitForm() {
-        if (this.registerForm.password !== this.registerForm.confirmPassword) {
-          this.$message.error('两次输入的密码不一致');
-          return;
-        }
-        // 发送到服务器的注册请求
-        console.log('注册信息:', this.registerForm);
-        // 这里可以添加 axios 请求发送数据到服务器
+  <div class="register-container">
+    <el-form ref="registerForm" :model="registerForm" :rules="rules" label-width="100px">
+      <el-form-item label="用户名">
+        <el-input v-model="registerForm.username" ></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="registerForm.email" type="email"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="registerForm.password"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码">
+        <el-input type="password" v-model="registerForm.confirmPassword"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">注册</el-button>
+        <el-button @click="resetForm('registerForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    registerForm: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+      }
+    };
+  },
+  rules: {
+    username: [
+      { required: true, message: '请输入用户名', trigger: 'blur' },
+      { min: 3, max: 10, message: '用户名长度在 3 到 10 个字符', trigger: 'blur' }
+    ],
+    email: [
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+    ],
+    password: [
+      { required: true, message: '请输入密码', trigger: 'blur' },
+      { min: 6, max: 16, message: '密码长度在 6 到 16 个字符', trigger: 'blur' }
+    ],
+    confirmPassword: [
+        { required: true, message: '请再次输入密码', trigger: 'blur' },
+        { validator: (rule, value, callback) => {
+            if (value !== this.registerForm.password) {
+              callback(new Error('两次输入的密码不一致'));
+            } else {
+              callback();
+            }
+          }, trigger: 'blur' }
+      ]
+  },
+  methods: {
+    submitForm() {
+        this.$refs.registerForm.validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
       },
       resetForm() {
         this.$refs.registerForm.resetFields();
-      }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .register-container {
-    max-width: 400px;
-    margin: 50px auto;
-    padding: 20px;
-    border: 1px solid #ebeef5;
-    border-radius: 4px;
   }
-  
-  .el-form-item {
-    margin-bottom: 20px;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.register-container {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+}
+
+.el-form-item {
+  margin-bottom: 20px;
+}
+</style>
