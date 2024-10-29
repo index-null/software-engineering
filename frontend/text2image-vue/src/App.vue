c1e27c12fd8c80108c3ba5a639e14bdeb5d54429
lind003 <127481264+lind003@users.noreply.github.com>
2024-10-29 14:37:59 +0800

    新添登陆界面
    登陆界面，登陆界面和注册界面的跳转

--- frontend/text2image-vue/src/App.vue
+++ frontend/text2image-vue/src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/" class="nav-link">Home</router-link> 
       <router-link to="/about" class="nav-link">About</router-link>
+      <router-link to="/login" class="nav-link">Login</router-link>
       <router-link to="/register" class="nav-link">Register</router-link>
     </nav>
     <router-view/>
--- frontend/text2image-vue/src/views/RegisterView.vue
+++ frontend/text2image-vue/src/views/RegisterView.vue
@@ -1,104 +1,116 @@
 <template>
-  <div class="register-container">
-    <h1>注册</h1>
-    <el-form ref="registerForm" :model="registerForm" :rules="rules" label-width="100px">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="registerForm.username" ></el-input>
-      </el-form-item>
-      <el-form-item label="邮箱" prop="email">
-        <el-input v-model="registerForm.email" type="email"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="registerForm.password"  autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="confirmPassword">
-        <el-input type="password" v-model="registerForm.confirmPassword"  autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">注册</el-button>
+  <div>
+    <el-card class="box-card">
+      <h2>注册</h2>
+      <el-form
+        :model="registerForm"
+        status-icon
+        :rules="rules"
+        ref="registerForm"
+        label-position="left"
+        label-width="80px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="uname">
+          <el-input v-model="registerForm.uname"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input
+            type="password"
+            v-model="registerForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="password">
+          <el-input
+            type="password"
+            v-model="registerForm.password"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="btnGroup">
+        <el-button type="primary" @click="submitForm('registerForm')"
+          >提交</el-button
+        >
         <el-button @click="resetForm('registerForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+        <el-button @click="goBack">返回</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    registerForm: {
-      username: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-      }
-    };
-  },
-  rules: {
-    username: [
-      { required: true, message: '请输入用户名', trigger: 'blur' },
-      { min: 3, max: 10, message: '用户名长度在 3 到 10 个字符', trigger: 'blur' }
-    ],
-    email: [
-      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
-    ],
-    password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
-    { validator: this.validatePass, trigger: 'blur' }
-    ],
-    confirmPassword: [
-    { required: true, message: '请再次输入密码', trigger: 'blur' },
-    { validator: this.validatePass2, trigger: 'blur' }
-      ]
-  },
-  methods: {
-    validatePass(rule, value, callback) {
+    var validatePass = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请输入密码"));
       } else {
-        if (this.registerForm.password !== "") {
-          this.$refs.registerForm.validateField("confirmPassword");
+        if (this.registerForm.checkPass !== "") {
+          this.$refs.registerForm.validateField("checkPass");
         }
         callback();
       }
-    },
-    validatePass2(rule, value, callback) {
+    };
+    var validatePass2 = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请再次输入密码"));
-      } else if (value !== this.registerForm.password) {
+      } else if (value !== this.registerForm.pass) {
         callback(new Error("两次输入密码不一致!"));
       } else {
         callback();
       }
-    },
-    submitForm() {
-      this.$refs.registerForm.validate((valid) => {
+    };
+    return {
+      registerForm: {
+        uname: "",
+        pass: "",
+        password: "",
+      },
+      rules: {
+        uname: [
+          { required: true, message: "用户名不能为空！", trigger: "blur" },
+        ],
+        pass: [{ required: true, validator: validatePass, trigger: "blur" }],
+        password: [
+          { required: true, validator: validatePass2, trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('submit!');
+          alert("submit!");
         } else {
-          console.log('error submit!!');
+          console.log("error submit!!");
           return false;
         }
       });
     },
-      resetForm() {
-        this.$refs.registerForm.resetFields();
-    }
-  }
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    goBack() {
+      //返回登陆界面
+      if(this.$router.path!=='/login'){
+        this.$router.push('/login');
+      }else{
+        this.$router.go(-1);
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-.register-container {
-  max-width: 400px;
-  margin: 50px auto;
-  padding: 20px;
-  border: 1px solid #ebeef5;
-  border-radius: 4px;
+.box-card {
+  margin: auto auto;
+  width: 400px;
 }
-
-.el-form-item {
-  margin-bottom: 20px;
+.login-from {
+  margin: auto auto;
 }
 </style>
