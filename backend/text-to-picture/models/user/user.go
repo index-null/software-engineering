4371406e9f70802c69b0549ace3925c789756883
czh01 <2680728792@qq.com>
2024-11-18 22:46:48 +0800

    #

--- backend/text-to-picture/models/user/user.go
+++ backend/text-to-picture/models/user/user.go
@@ -5,7 +5,7 @@ import (
 )
 
 type UserInformation struct {
-	ID          int       `json:"id" gorm:"primarykey"`
+	ID          int       `json:"id" gorm:"primarykey;autoIncrement"`
 	Email       string    `json:"email" gorm:"unique;not null"`
 	UserName    string    `json:"username" gorm:"column:username; not null"`
 	Password    string    `json:"password" gorm:"not null"`
@@ -13,7 +13,7 @@ type UserInformation struct {
 	Token       string    `json:"token"`
 	Create_time time.Time `json:"create_time"`
 }
-
+gti
 func (UserInformation) TableName() string {
 	return "userinformation"
 }
